779657d92662e87fc44d0d0deb1b0d86af1ff695
wjonke <wesjonke@gmail.com>
2020-05-08 11:18:59 -0400

    Initial commit, prepped challenges with readme content

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,5 +1,4 @@
 def intersection(arrays):
-
     """
     YOUR CODE HERE
     """
@@ -10,8 +9,33 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+# Find the intersection between multiple lists of integers.
+#
+# Do not use numpy or sets to solve this; use dict or hash-tables, please.
+#
+# We're given a list of lists that contain integers:
+#
+# [
+#     [1, 2, 3, 4, 5],
+#     [12, 7, 2, 9, 1],
+#     [99, 2, 7, 1,]
+# ]
+# And we need to compute the intersection, that is, numbers that exist in all lists.
+#
+# From the above input, the return value will be:
+#
+# [1, 2]
+# Because those are the numbers that exist in all the lists.
+#
+# (Output can be in any order.)
+#
+# Limits:
+#
+# There can be up to 10 lists in the list of lists.
+# The lists can contain up to roughly 1,000,000 elements each.
